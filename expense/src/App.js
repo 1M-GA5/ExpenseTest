a826b43c22e9097f5139f65b94099fedfff11691
1M-GA5 <athrvgajbhiyexxi@gmail.com>
2023-03-12 16:21:31 +0530

    First Commit

---
+++ expense/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ExpenseItems from './Components/ExpenseItems';
+function App() {
+  return (
+    <div className="App">
+      <ExpenseItems / >
+    </div>
+  );
+}
+
+export default App;
